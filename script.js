e522aa41ff5cea81cdd801ad59f6af2947f46dfc
higoramaral <amarahigor@gmail.com>
2023-12-11 15:28:01 -0300

    11/12

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ document.getElementById("tarefaInput").addEventListener("keypress", function (e)
 })
 //validar o item informado
 function adicionarTarefa() {
+    // Obtem o valor da id tarefaInput
     let tarefa = document.getElementById("tarefaInput").value;
+    // verificando se está vazia
     if (tarefa == '') {
         alert("Não deixe em branco.")
     }
     else {
-         // Cria um novo elemento <li> e adiciona o conteúdo do input a ele
+        // Cria um novo elemento <li> e adiciona o conteúdo do input a ele
         let li = document.createElement("li");
         li.innerHTML = tarefa;
         tarefa = document.getElementById("tarefaInput").value = ''
@@ -19,24 +21,32 @@ function adicionarTarefa() {
         //Botão de concluir
         let btnConcluir = document.createElement("button");
         btnConcluir.innerHTML = "Concluir";
-           // Remove o item da lista de tarefas pendentes e move para a lista de concluídas
+        // Remove o item da lista de tarefas pendentes e move para a lista de concluídas
         btnConcluir.onclick = function () {
+            // quando clicar ele remove o botão
             this.parentElement.remove();
+            // informações sobre o elemento
             console.log(this);
+            // cria uma variavel e amazena
             let itemConcluido = this.parentElement;
+            // remove o botão
             itemConcluido.removeChild(this);
+            // coloca como afiliado do itemConcluido
             document.getElementById("listaConcluidos").appendChild(itemConcluido);
 
             //Botão de excluir
+            // cria um botão
             let btnExcluir = document.createElement("button");
+            // defini como excluir
             btnExcluir.innerHTML = "Excluir";
+            // função de remover
             btnExcluir.onclick = function () {
                 this.parentElement.remove();
             };
             itemConcluido.appendChild(btnExcluir);
         };
         li.appendChild(btnConcluir);
-             // Adiciona o item à lista de tarefas pendentes
+        // Adiciona o item à lista de tarefas pendentes
         document.getElementById("listaTarefas").appendChild(li);
     }
 }
